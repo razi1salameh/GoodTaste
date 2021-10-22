8c03ca4918ab18c93ac4d4bddf8abf246149c70b
razi1salameh <razi1salameh@gmail.com>
2021-10-22 10:04:14 +0300

    aa

--- app/src/main/java/com/example/goodtaste/LogInActivity.java
+++ app/src/main/java/com/example/goodtaste/LogInActivity.java
@@ -2,7 +2,9 @@ package com.example.goodtaste;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class LogInActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class LogInActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+    }
+
+    public void LogIn(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
