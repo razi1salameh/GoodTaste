c90023fee14c16c4ecf51786823599a0a6784714
razi1salameh <razi1salameh@gmail.com>
2021-11-15 12:58:50 +0200

    improve the navigation bar to look better

--- app/src/main/java/com/example/goodtaste/CustomsAdapter.java
+++ app/src/main/java/com/example/goodtaste/CustomsAdapter.java
@@ -20,33 +20,35 @@ public class CustomsAdapter extends ArrayAdapter<Recipe> {
     private Context context;
     private int resource;
 
-    public CustomsAdapter(@NonNull Context context, int resource, @NonNull List<Recipe> objects) {
+    public CustomsAdapter(Context context, int resource, List<Recipe> objects) {
         super(context, resource, objects);
         this.context = context;
         this.resource = resource; //this the
     }
 
-    @NonNull
     @Override
-    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+    public View getView(int position, @Nullable View convertView, ViewGroup parent) {
         View view = convertView;
         if (view == null)
             view = LayoutInflater.from(context).inflate(resource, parent, false);
         Recipe recipe = getItem(position); //method from the android studio not related to the item object
         if (recipe != null) {
-            ImageView imageView = view.findViewById(R.id.imageItem);
-            TextView textViewDescription = view.findViewById(R.id.textViewDesc);
-            Button itemButton = view.findViewById(R.id.itemButton);
+            TextView textViewRecipeName = view.findViewById(R.id.textViewRecipeName);
+            ImageView imageViewRecipePicture = view.findViewById(R.id.imageViewRecipePicture);
+            Button buttonOpenRecipeToView = view.findViewById(R.id.buttonOpenRecipeToView);
+            Button buttonViewCreator = view.findViewById(R.id.buttonViewCreator);
+            Button buttonViewVideo = view.findViewById(R.id.buttonViewVideo);
+            Button buttonViewSave = view.findViewById(R.id.buttonViewSave);
 
-            itemButton.setOnClickListener(new View.OnClickListener() {
+
+            buttonOpenRecipeToView.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
                     Toast.makeText(context, "This item was added to shopping cart", Toast.LENGTH_LONG).show();
                 }
             });
-
-            imageView.setImageResource(recipe.getResid());
-            textViewDescription.setText(recipe.getDescription());
+            //NEED TO ADD THE OTHER PROPERTIES OT THE OBJECT AND ADD THE OBJECT USER (CREATOR)
+            textViewRecipeName.setText(recipe.getTitle());
         }
         return view;
     }
--- app/src/main/java/com/example/goodtaste/NavDrawerActivity.java
+++ app/src/main/java/com/example/goodtaste/NavDrawerActivity.java
@@ -41,9 +41,7 @@ public class NavDrawerActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
-                .setOpenableLayout(drawer)
-                .build();
+                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow).setOpenableLayout(drawer).build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_nav_drawer);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
